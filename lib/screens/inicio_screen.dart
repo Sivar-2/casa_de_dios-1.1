6b343853741ea995b9b0644162e52dd8680ffb3b
Sivar-2 <alexander7tm@gmail.com>
2026-03-04 01:00:19 -0600

    Fotos actualizadas

--- lib/screens/inicio_screen.dart
+++ lib/screens/inicio_screen.dart
@@ -68,7 +68,7 @@ class _InicioScreenState extends State<InicioScreen> {
         backgroundColor: Colors.white,
         elevation: 0,
         title: Image.asset(
-          'assets/images/lo_go.png', 
+          '/assets/images/lo_go.png', 
           height: 35,
           fit: BoxFit.contain,
         ),
