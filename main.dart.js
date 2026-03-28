7ba097ba16e616615071811d69aa66833abd2167
Sivar-2 <Sivar-2@users.noreply.github.com>
2026-03-28 00:34:07 +0000

    deploy: b19a3fe62dc8c61bb0bb80594f5fb725fdb66af9

--- main.dart.js
+++ main.dart.js
@@ -31556,7 +31556,7 @@ $S:2}
 A.aCv.prototype={
 $1(a){var s=A.dK().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/e4b8dca3f1b4ede4c30371002441c88c12187ed6/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/425cfb54d01a9472b3e81d9e76fd63a4a44cfbcb/":s)+a},
 $S:29}
 A.Py.prototype={
 gv(a){var s=this.a
